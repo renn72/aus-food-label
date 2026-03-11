5be02beeac9259e71a53769c3a7ed363a7b4e1cf
David <git@warner.systems>
2026-03-11 11:17:28 +1100

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+import { IntroPageDeleteMe } from "@/components/_DELETE_ME_intro_page";
+
+export const Route = createFileRoute("/")({
+  component: HomePage,
+});
+
+function HomePage() {
+  // This is the intro component for TanStarter, which you may delete after creating the project.
+  // Have fun!
+  return <IntroPageDeleteMe />;
+}
